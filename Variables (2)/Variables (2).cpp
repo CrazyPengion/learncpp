d758c613697d5bea6238c3a04ca9326e0632e3d6
CrazyPengion <118538266+CrazyPengion@users.noreply.github.com>
2026-05-31 19:56:25 +0200

    Variables (2) - Final
    Wrote own simple program at the end

--- Variables (2)/Variables (2).cpp
+++ Variables (2)/Variables (2).cpp
@@ -1,16 +1,11 @@
-#include <iostream>  // for std::cout and std::cin
+#include <iostream>
 
 int main()
 {
-    std::cout << "Enter 3 numbers: "; // ask user for a number
-    int x{}; // define variable x to hold user input
-	int y{}; // define variable y to hold user input
-	int z{}; // define variable z to hold user input
+	int x{0};
+	std::cout << "Enter an integer: ";
+	std::cin >> x;
+	std::cout << "Double that number is: " << x * 2;
 
-    std::cin >> x;
-    std::cin >> y; // get number from keyboard and store it in variable y
-    std::cin >> z;
-    std::cout << "You entered " << x << ", " << y << ", and " << z << "." << '\n';
-
-    return 0;
+	return 0;
 }
